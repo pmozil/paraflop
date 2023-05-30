b47bc6cbd78396607a0185e5474c2fbd399a2cab
Petro Mozil <mozil.petryk@gmail.com>
2023-05-30 15:47:07 +0300

    Figure out the camera

--- include/geometry/camera.h
+++ include/geometry/camera.h
@@ -10,7 +10,7 @@ const float FOV = glm::radians(90.0F);
 struct Camera {
     float fov = FOV;
     float focus = FOCUS;
-    float focal_len = focal_len;
+    float focal_len = FOCAL_LEN;
     glm::vec3 position;
     glm::vec3 rotation;
     glm::vec3 up;
@@ -19,7 +19,7 @@ struct Camera {
     transformMatrices(float width, float height) const {
         UniformBufferObject ubo{};
 
-        ubo.model = glm::identity<glm::mat4>();
+        ubo.model = glm::lookAt(position, position + rotation, up);
         ubo.view = glm::translate(glm::mat4(1.0F), -position);
         ubo.proj = glm::perspective(fov, width / height, focal_len, focus);
 

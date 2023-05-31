cbb7a759929ecbabc46ef2b8bd8ad04702f65828
Petro Mozil <mozil.petryk@gmail.com>
2023-05-31 13:57:01 +0300

    Extract GLFW callbacks into separate file

---
+++ include/interface/glfw_callbacks.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "geometry/camera.h"
+
+struct AppState {
+    std::shared_ptr<geometry::Camera> camera;
+    float timePassed = 0.0F;
+    float mouseX = 0.0F;
+    float mouseY = 0.0F;
+
+    AppState(std::shared_ptr<geometry::Camera> cam) : camera(std::move(cam)) {}
+};
+
+void handleKeyPress(GLFWwindow *window, int key, int sancode, int action,
+                    int mods);
+void handleFocus(GLFWwindow *window, int focused);
+
+void handleCursor(GLFWwindow *window, double xpos, double ypos);
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "geometry/uniform_buffer_object.hpp"
 #include "geometry/vertex.hpp"
 #include "interface/custom_graphics_pipeline.hpp"
+#include "interface/glfw_callbacks.hpp"
 #include "interface/renderer.hpp"
 #include "vulkan_utils/buffer.hpp"
 #include "vulkan_utils/command_buffer.hpp"
@@ -18,15 +19,6 @@
 #include "vulkan_utils/vk_instance.hpp"
 #include "vulkan_utils/window.hpp"
 
-struct AppState {
-    std::shared_ptr<geometry::Camera> camera;
-    float timePassed = 0.0F;
-    float mouseX = 0.0F;
-    float mouseY = 0.0F;
-
-    AppState(std::shared_ptr<geometry::Camera> cam) : camera(std::move(cam)) {}
-};
-
 const std::vector<geometry::Vertex> vertices = {
     {{-0.5F, -0.5F, 0.0F}, {1.0F, 0.0F, 0.0F}},
     {{0.5F, -0.5F, 0.0F}, {0.0F, 1.0F, 0.0F}},
@@ -34,71 +26,6 @@ const std::vector<geometry::Vertex> vertices = {
     {{-0.5F, 0.5F, 0.0F}, {1.0F, 1.0F, 1.0F}}};
 
 const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
-
-void handleKeyPress(GLFWwindow *window, int key, int sancode, int action,
-                    int mods) {
-    auto *cam = (AppState *)glfwGetWindowUserPointer(window);
-    if (key == GLFW_KEY_W) {
-        cam->camera->moveForward(cam->timePassed);
-    }
-    if (key == GLFW_KEY_S) {
-        cam->camera->moveForward(-cam->timePassed);
-    }
-    if (key == GLFW_KEY_A) {
-        cam->camera->moveLeft(cam->timePassed);
-    }
-    if (key == GLFW_KEY_D) {
-        cam->camera->moveLeft(-cam->timePassed);
-    }
-    if (key == GLFW_KEY_Q) {
-        cam->camera->moveUp(cam->timePassed);
-    }
-    if (key == GLFW_KEY_E) {
-        cam->camera->moveUp(-cam->timePassed);
-    }
-}
-
-void handleFocus(GLFWwindow *window, int focused) {
-    double xpos;
-    double ypos;
-    int width;
-    int height;
-
-    glfwGetWindowSize(window, &width, &height);
-    glfwGetCursorPos(window, &xpos, &ypos);
-
-    auto *cam = (AppState *)glfwGetWindowUserPointer(window);
-
-    if (static_cast<bool>(focused)) {
-        cam->mouseX = xpos / width;
-        cam->mouseY = ypos / height;
-    }
-}
-
-void handleCursor(GLFWwindow *window, double xpos, double ypos) {
-    int width;
-    int height;
-
-    glfwGetWindowSize(window, &width, &height);
-
-    auto *cam = (AppState *)glfwGetWindowUserPointer(window);
-
-    float relX = xpos / width;
-    float relY = ypos / height;
-
-    float diffX = cam->mouseX - relX;
-    float diffY = relY - cam->mouseY;
-
-    if ((cam->mouseX != 0.0 || cam->mouseY != 0.0) &&
-        (diffX * diffX + diffY * diffY < 1)) {
-        cam->camera->calcTurn(diffX * cam->camera->focus * cam->camera->focus,
-                              diffY * cam->camera->focus * cam->camera->focus);
-    }
-    cam->mouseX = relX;
-    cam->mouseY = relY;
-
-    glfwSetCursorPos(window, width / 2, height / 2);
-}
 
 int main() {
     std::vector<const char *> validation = {"VK_LAYER_KHRONOS_validation"};
